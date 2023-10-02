ed5a6d891855506dbf2bde38aa43009910206b3b
Pedro Souza <pedrops244@hotmail.com>
2023-10-02 15:36:12 -0300

    type-array

--- src/A005-type-array/index.ts
+++ src/A005-type-array/index.ts
@@ -1,3 +1,12 @@
-function multiplyArgs(...args: Array<number>) {
+export function multiplyArgs(...args: Array<number>): number {
   return args.reduce((acc, valor) => acc * valor, 1);
 }
+export function toUpperCase(...args: string[]): string[] {
+  return args.map((valor) => valor.toUpperCase());
+}
+
+const result = multiplyArgs(1, 2, 3);
+const resultconcat = toUpperCase('Pedro', 'Souza', 'OILA');
+
+console.log(resultconcat);
+console.log(result);
