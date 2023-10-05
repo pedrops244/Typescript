5ec86a4ff84fa7b2622c2de22cf97d7505d744ae
Pedro Souza <pedrops244@hotmail.com>
2023-10-05 20:35:58 -0300

    config webpack

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!***********************************!*\
+  !*** ./src/A018-webpack/index.ts ***!
+  \***********************************/
+
+console.log('Oi');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
