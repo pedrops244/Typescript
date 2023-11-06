fa510478840c8c98de2bfb27b7199ff9a7ba0111
Pedro Souza <pedrops244@hotmail.com>
2023-11-06 18:26:00 -0300

    this-polymorphism

---
+++ src/A040-this-polymorphism/index.ts
@@ -0,0 +1,55 @@
+export class Calculadora {
+  constructor(public number: number) {}
+
+  add(n: number): this {
+    this.number += n;
+    return this;
+  }
+  sub(n: number): this {
+    this.number -= n;
+    return this;
+  }
+  div(n: number): this {
+    this.number /= n;
+    return this;
+  }
+  mul(n: number): this {
+    this.number *= n;
+    return this;
+  }
+}
+
+export class SubCalculadora extends Calculadora {
+  pow(n: number): this {
+    this.number **= n;
+    return this;
+  }
+}
+
+const calculadora = new SubCalculadora(10);
+calculadora.add(5).mul(2).div(2).sub(5).pow(2);
+console.log(calculadora);
+
+///Builder
+
+export class Request {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Enviando dados via ${this.method} para ${this.url}`);
+  }
+}
+
+const request = new Request();
+request.setUrl('http://www.google.com').setMethod('post').send();
